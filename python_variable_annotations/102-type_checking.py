48eca8fc4a43466d3c1b299b2de897ad6b0db08f
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2023-08-08 17:47:18 +0000

    Fixed 102 for mypy

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Type Checking"""
-from typing import Tuple, List, Any, Union
+from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
@@ -10,3 +10,10 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
         for i in range(factor)
     ]
     return zoomed_in
+
+
+array: Tuple = (12, 72, 91)
+
+zoom_2x: List = zoom_array(array)
+
+zoom_3x: List = zoom_array(array, 3)
