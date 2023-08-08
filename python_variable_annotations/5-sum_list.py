2f20799a1e6d862e7c6df3c8b6ce3945a81577fe
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2023-08-08 17:59:29 +0000

    Fixed typo

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Complex types - list of floats"""
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Sum of all floats in a list"""
     return sum(input_list)
