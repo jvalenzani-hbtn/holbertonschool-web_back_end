6a51790f9c5b5d048e7fcea24c4b337a7f39ea4c
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2023-08-08 17:55:37 +0000

    Pystyle fix

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Complex types - mixed list"""
 
-from typing import Union 
+from typing import Union
 
 
 def sum_mixed_list(mixed_list: list[Union[int, float]]) -> float:
