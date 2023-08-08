ad1d11117479ad351845d319be20967b3e42294c
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2023-08-08 18:00:50 +0000

    Fixed Typo

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Complex types - mixed list"""
 
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mixed_list: list[Union[int, float]]) -> float:
+def sum_mixed_list(mixed_list: List[Union[int, float]]) -> float:
     """Sum of all floats in a list"""
     return sum(mixed_list)
