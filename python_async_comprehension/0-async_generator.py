3851a03a6043cffc05f3850195378b40a10c4ba6
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2023-08-24 19:10:01 +0000

    Fixed return annotation.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,11 @@
 
 import asyncio
 import random
+import typing
 
 
-async def async_generator() -> int:
-    """ Async Generator """
+async def async_generator() -> typing.Generator[float, None, None]:
+    """ Async Generator (Returns Generator type due to the yield expr.) """
     for _ in range(10):
         yield random.uniform(0, 10)
         await asyncio.sleep(1)
