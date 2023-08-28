29ebb4063b43d7775d9af7a30f764305bb0dc088
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2023-08-28 18:31:06 +0000

    Fixed typing import

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,10 @@
 """ Async Comprehensions """
 
 import asyncio
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> typing.List[float]:
     """ Async Comprehensions """
     return [i async for i in async_generator()]
